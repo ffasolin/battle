990608d81c86375fb446f0b7ae32f0a0a03e7610
Adam Snow <adamjohnsnow@icloud.com>
2017-04-26 13:47:43 +0100

    added hit points

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,8 +1,10 @@
 feature 'home page' do
-    scenario 'should return string in home page' do
+
+    scenario 'should return names & hit points' do
       visit('/')
-      fill_in('name', with: 'Filipe')
+      fill_in('player_1', with: 'Filipe')
+      fill_in('player_2', with: 'Filipe')
       click_button('Submit')
-      expect(page).to have_content('Filipe')
+      expect(page).to have_content('Filipe 60pts vs. Filipe 60pts')
     end
 end
